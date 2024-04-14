20edb66a278c2aad936f36ddbe04823544c2e3a8
Shane Gadsby <schme16@gmail.com>
2024-04-14 20:10:51 +1000

    staging

--- Assets/data/scripts/PlayerScript.cs
+++ Assets/data/scripts/PlayerScript.cs
@@ -10,6 +10,7 @@ public class PlayerScript : MonoBehaviour {
 	public Transform circle;
 	public Transform spawnPoint;
 	public Transform armature;
+	public Transform oldGuyNpc;
 	public Transform[] spawnPoints;
 	public Vector3 spawnPointOffset;
 	public Vector3 armatureZeroPos;
@@ -17,6 +18,7 @@ public class PlayerScript : MonoBehaviour {
 	public Vector3 circleStartingEuler;
 	public bool isAnimating;
 	public bool isInDialogue;
+	public StarterAssetsInputs inputs;
 
 	public ThirdPersonController tpController;
 	public PlayerInput playerInput;
@@ -30,7 +32,7 @@ public class PlayerScript : MonoBehaviour {
 	public CinemachineTargetGroup cTargets;
 
 	public DialogueEntry dialogue;
-	
+
 
 	private Vector3 cameraTargetStartPos;
 	private bool isAnimatingLast = true;
@@ -46,6 +48,7 @@ public class PlayerScript : MonoBehaviour {
 		circleStartingEuler = circle.localEulerAngles;
 		cameraTargetStartPos = cameraTarget.localPosition;
 		cTargets.m_Targets[0].target = transform;
+		Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	// Update is called once per frame
@@ -66,8 +69,8 @@ public class PlayerScript : MonoBehaviour {
 			}
 
 			if (!circleAnim.GetCurrentAnimatorStateInfo(0).IsName("idle-down") && Input.GetKeyDown(KeyCode.P)) {
-				isAnimating = true;
-				circleAnim.SetTrigger("leaving");
+				/*isAnimating = true;
+				circleAnim.SetTrigger("leaving");*/
 			}
 		}
 
@@ -79,16 +82,23 @@ public class PlayerScript : MonoBehaviour {
 			//Turn the player controls off if in dialogue, or animating
 			if (isAnimating) {
 				playerInput.SwitchCurrentActionMap("Dialogue");
+				playerInput.DeactivateInput();
 				switchToCamera(entryAndExitCamera);
 			}
-			else if (isInDialogue && !!DialogueTarget) {
+
+			else if (isInDialogue && DialogueTarget) {
 				playerInput.SwitchCurrentActionMap("Dialogue");
-				armature.LookAt(DialogueTarget);
+				playerInput.DeactivateInput();
+
+				transform.LookAt(
+					new Vector3(DialogueTarget.position.x, transform.position.y, DialogueTarget.position.z));
+
 				switchToCamera(dialogueCamera);
 			}
 
 			//Otherwise turn them back on
 			else {
+				playerInput.ActivateInput();
 				playerInput.SwitchCurrentActionMap("Player");
 				switchToCamera(playerFollowCamera);
 			}
@@ -97,8 +107,8 @@ public class PlayerScript : MonoBehaviour {
 		circle.localEulerAngles = new Vector3(circleStartingEuler.x, armature.localEulerAngles.y,
 			circleStartingEuler.z + circleStartingEuler.z);
 
-		if (Input.GetKeyDown(KeyCode.I)) {
-			isInDialogue = true;
+		if (!isInDialogue && Input.GetKeyDown(KeyCode.I)) {
+			DialogueManager.StartConversation("Not interested", armature, oldGuyNpc, 0);
 		}
 	}
 
@@ -108,5 +118,41 @@ public class PlayerScript : MonoBehaviour {
 		dialogueCamera.enabled = false;
 
 		cam.enabled = true;
+	}
+
+	public void StartDialogue() {
+		isInDialogue = true;
+		Cursor.lockState = CursorLockMode.Confined;
+	}
+
+	public void EndDialogue() {
+		isInDialogue = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		DialogueManager.StopAllConversations();
+	}
+
+	public void QuestFailed() {
+		var state = QuestLog.CurrentQuestState("QuestFailed");
+		if (state == "active") {
+			QuestLog.SetQuestState("QuestFailed", QuestState.Unassigned);
+			isAnimating = true;
+			circleAnim.SetTrigger("leaving");
+		}
+	}
+
+	public void QuestMyHeart() {
+		var state = QuestLog.CurrentQuestState("QuestMyHeart");
+		if (state == "active") {
+			QuestLog.SetQuestState("QuestFailed", QuestState.Unassigned);
+			isAnimating = true;
+			circleAnim.SetTrigger("leaving");
+		}
+	}
+
+	public void StartFailedConversation() {
+		var state = QuestLog.CurrentQuestState("QuestFailed");
+		if (state == "active") {
+			DialogueManager.StartConversation("FailedSpeech", armature, armature, 0);
+		}
 	}
 }
